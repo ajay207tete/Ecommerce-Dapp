10d9049f62674ec6574b35596bf06f2490e807cf
ajay207tete <101888582+ajay207tete@users.noreply.github.com>
2026-03-30 13:02:49 +0530

    Update ProductDetail.js

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -7,7 +7,7 @@ import { ShoppingCart, Plus, ChevronLeft, ChevronRight } from 'lucide-react';
 import { useCart } from '../contexts/CartContext';
 import { toast } from 'sonner';
 
-const API = `${process.env.REACT_APP_BACKEND_URL}/api';
+const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
 
 const ProductDetail = () => {
   const { id } = useParams();
