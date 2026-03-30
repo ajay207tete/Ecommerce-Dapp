270f5a0524d8048816f73f21916b6d7d1e25f86a
ajay207tete <101888582+ajay207tete@users.noreply.github.com>
2026-03-30 16:59:16 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -799,7 +799,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["https://ecommerce-dapp-i9u9.vercel.app/",
+    allow_origins=["https://ecommerce-dapp-i9u9.vercel.app",
 "http://localhost:3000",
     "http://localhost:19006"],
     allow_methods=["*"],
