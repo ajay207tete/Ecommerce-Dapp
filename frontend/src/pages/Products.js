dd1d58be07ebea7350a9458091e47c8a8fa24ea2
ajay207tete <101888582+ajay207tete@users.noreply.github.com>
2026-03-30 12:28:45 +0530

    Update Products.js

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -7,8 +7,7 @@ import { Plus, ShoppingCart } from 'lucide-react';
 import { useCart } from '../contexts/CartContext';
 import { toast } from 'sonner';
 
-const API="https://thruster-dapp.onrender.com/api";
-
+const API='${process.env.REACT_APP_BACKEND_URL}/api';
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
