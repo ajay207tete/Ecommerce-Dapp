04dda3c81b15bfd29d9588f7b51d3cd23bbd5094
ajay207tete <101888582+ajay207tete@users.noreply.github.com>
2026-04-12 05:38:19 +0530

    Update helpers.ts

--- smart-contract/utils/helpers.ts
+++ smart-contract/utils/helpers.ts
@@ -1,44 +1,51 @@
-import { TonClient4, WalletContractV4, internal } from '@ton/ton';
+import { TonClient4, WalletContractV4 } from '@ton/ton';
 import { mnemonicToPrivateKey } from '@ton/crypto';
+import { beginCell, internal, Sender, toNano } from '@ton/core';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const TESTNET = {
-  clientConfig: {
-    endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
-    apiKey: process.env.TONCENTER_API_KEY // Optional
-  }
-};
-
 export async function getTonClient() {
   return new TonClient4({
-    endpoint: TESTNET.clientConfig.endpoint,
-    apiKey: TESTNET.clientConfig.apiKey
+    endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+    apiKey: process.env.TONCENTER_API_KEY
   });
 }
 
 export async function getSender(mnemonic: string = process.env.MNEMONIC!) {
-  const keyPair = await mnemonicToPrivateKey(mnemonic.split(' '));
-  const wallet = WalletContractV4.create({ workchain: 0, publicKey: keyPair.publicKey });
   const client = await getTonClient();
-  
-  return {
-    wallet,
-    sender: internal({
-      from: wallet.address,
-      deploy: !!wallet.code,
-      body: '',
-      bounce: false,
-      value: '0.1'
-    }, keyPair)
-  };
-}
 
-export function collectionConfig(metadataUrl: string) {
+  const keyPair = await mnemonicToPrivateKey(mnemonic.split(' '));
+
+  const wallet = WalletContractV4.create({
+    workchain: 0,
+    publicKey: keyPair.publicKey
+  });
+
+  const walletContract = client.open(wallet);
+
+  // ✅ CORRECT sender
+  const sender: Sender = {
+    send: async (args) => {
+      const seqno = await walletContract.getSeqno();
+
+      await walletContract.sendTransfer({
+        secretKey: keyPair.secretKey,
+        seqno,
+        messages: [
+          internal({
+            to: args.to!,
+            value: args.value || toNano('0.1'),
+            body: args.body || beginCell().endCell(),
+            bounce: false
+          })
+        ]
+      });
+    }
+  };
+
   return {
-    testOnly: true,
-    code: 'NftCollection',
-    initialValues: { ownerAddress: 'EQ...', nextItemIndex: 0n, content: metadataUrl.toCell() }
+    wallet: walletContract,
+    sender
   };
 }
