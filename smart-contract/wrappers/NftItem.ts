eb5c5da7dca52587e02b58e030f9d4a282e4932c
ajay207tete <101888582+ajay207tete@users.noreply.github.com>
2026-04-12 05:35:51 +0530

    Update NftItem.ts

--- smart-contract/wrappers/NftItem.ts
+++ smart-contract/wrappers/NftItem.ts
@@ -1,59 +1,44 @@
-import { Address, beginCell, Cell, Contract, ContractProvider, Sender, Slice } from '@ton/core';
-
-export type NftItemData = {
-  init?: Address;
-  index: bigint;
-  collection: Address;
-  owner: Address;
-  content: Cell;
-};
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  ContractProvider,
+  Sender
+} from '@ton/core';
 
 export class NftItem implements Contract {
-  constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+  constructor(readonly address: Address) {}
 
   static createFromAddress(address: Address) {
     return new NftItem(address);
   }
 
-  static createFromConfig(config: NftItemData, code: Cell, workchain = 0) {
-    const data = NftItem.packData(config);
-    return new NftItem(contractAddress(workchain, { code, data }));
-  }
-
-  async sendTransfer(provider: ContractProvider, via: Sender, toAddress: Address, value = toNano('0.05')) {
+  async sendTransfer(
+    provider: ContractProvider,
+    via: Sender,
+    to: Address,
+    value = 50000000n
+  ) {
     await provider.internal(via, {
       value,
       body: beginCell()
-        .storeUint(0x5fcc3d14, 32) // transfer
-        .storeAddress(toAddress)
-        .storeRef(beginCell().storeUint(0, 64).endCell()) // response_dest null
-        .storeUint(0, 64) // query_id
-        .storeRef(beginCell().endCell()) // custom_payload empty
-        .endCell(),
+        .storeUint(0x5fcc3d14, 32)
+        .storeAddress(to)
+        .endCell()
     });
   }
 
   async getNftData(provider: ContractProvider) {
-    const result = provider.get('get_nft_data', []) as Cell;
-    const data = NftItemData.unpack(result.beginParse());
-    return data;
+    const res = await provider.get('get_nft_data', []);
+    const slice = res.stack.readCell().beginParse();
+
+    slice.loadDict(); // skip dict
+    const content = slice.loadRef();
+    const id = slice.loadUintBig(64);
+    const collection = slice.loadAddress();
+    const owner = slice.loadAddress();
+
+    return { id, collection, owner, content };
   }
-}
-
-export function packNftItemData(data: NftItemData): Cell {
-  return beginCell()
-    .storeUint(data.index, 64)
-    .storeAddress(data.collection)
-    .storeRef(data.content)
-    .storeAddress(data.owner)
-    .endCell();
-}
-
-export function unpackNftItemData(slice: Slice): NftItemData {
-  return {
-    index: slice.loadUintBig(64),
-    collection: slice.loadAddress(),
-    content: slice.loadRef(),
-    owner: slice.loadAddress(),
-  };
 }
